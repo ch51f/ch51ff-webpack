d34d1be0d7a3abdb6b5d77652c3fc222f9ab673a
ch51ff <371476909@qq.com>
2016-05-26 18:04:06 +0800

    router

--- router/main.js
+++ router/main.js
@@ -1,24 +1,106 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {render} from 'react-dom'
-import {Router, hashHistory} from 'react-router'
-import stubbedCourses from './stubs/COURSES'
+import {hashHistory, Router, Route, Link} from 'react-router'
+import data from './data'
+import './main_15.css'
 
-const rootRoute = {
-	component: 'div',
-	childRoutes: [{
-		path: '/',
-		component: require('./components/AppA'),
-		childRoutes: [
-			require('./routers/Calendar'),
-			require('./routers/Course'),
-			require('./routers/Grades'),
-			require('./routers/Messages'),
-			require('./routers/Profile')
-		]
-	}]
+class Category extends Component {
+	render() {
+		const category = data.lookupCategory(this.props.params.category)
+		return (
+			<div>
+				<h1>{category.name}</h1>
+				{this.props.children || (<p>{category.description}</p>)}
+			</div>
+		)
+	}
 }
 
-render(
-	<Router history={hashHistory} routes={rootRoute} />,
-	document.getElementById('app')
-)
+class CategorySidebar extends Component {
+	render() {
+		const category = data.lookupCategory(this.props.params.category)
+		return (
+			<div>
+				<Link to="/">◀︎ Back</Link>
+				<h2>{category.name} Items</h2>
+				<ul>
+					{category.items.map((item, index) => (
+						<li key={index}>
+							<Link to={`/category/${category.name}/${item.name}`}>{item.name}</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		)
+	}
+}
+
+class Item extends Component {
+	render() {
+		const {category, item} = this.props.params
+		const menuItem = data.lookupItem(category, item)
+		return (
+			<div>
+				<h1>{menuItem.name}</h1>
+				<p>{menuItem.price}</p>
+			</div>
+		)
+	}
+}
+
+class Index extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Sidebar</h1>
+				<p>
+					Routes can have multiple components, so that all portions of your UI
+					can participate in the routing.
+				</p>
+			</div>
+		)
+	}
+}
+
+class IndexSidebar extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Categories</h2>
+				<ul>
+					{data.getAll().map((category, index) => (
+						<li key={index}>
+							<Link to={`/category/${category.name}`}>{category.name}</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		)
+	}
+}
+
+class App extends Component {
+	render() {
+		const {content, sidebar} = this.props
+		return (
+			<div>
+				<div className="Sidebar">
+					{sidebar || <IndexSidebar />}
+				</div>
+				<div className="Content">
+					{content || <Index />}
+				</div>
+			</div>
+		)
+	}
+}
+
+render((
+	<Router history={hashHistory}>
+		<Route path="/" component={App}>
+			<Route path="category/:category" components={{content: Category, sidebar: CategorySidebar}}>
+				<Route path=":item" component={Item} />
+			</Route>
+		</Route>
+	</Router>
+), document.getElementById('app'))
---
+++ router/main_8.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {Router, hashHistory} from 'react-router'
+import stubbedCourses from './stubs/COURSES'
+
+const rootRoute = {
+	component: 'div',
+	childRoutes: [{
+		path: '/',
+		component: require('./components/AppA'),
+		childRoutes: [
+			require('./routers/Calendar'),
+			require('./routers/Course'),
+			require('./routers/Grades'),
+			require('./routers/Messages'),
+			require('./routers/Profile')
+		]
+	}]
+}
+
+render(
+	<Router history={hashHistory} routes={rootRoute} />,
+	document.getElementById('app')
+)
