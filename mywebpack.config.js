8527a06c06aa960e5692af32477f9b6637435c95
ch51ff <371476909@qq.com>
2016-05-31 17:54:20 +0800

    杂七杂八

---
+++ mywebpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack')
+
+module.exports = {
+	devtool: 'cheap-module-eval-source-map',
+	entry: [
+		'webpack-hot-middleware/client',
+		'./redux/index'
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/static/'
+	},
+	plugins: [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	],
+	module: {
+		loaders: [
+			{test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel', query: {presets: ['es2015', 'react']}},
+		]
+	}
+}
